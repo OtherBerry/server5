ef0b03e4f5c1c3b22739bc6571a5e71bf407a7f9
gilgy1 <gilgy@post.bgu.ac.il>
2024-04-12 17:08:15 +0300

    de video

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { Pool } = require('pg')
 
-var video_to_reroute = 'https://www.sport5.co.il/';
+var video_to_reroute = 'https://youtu.be/HNU7o3_h87U';
 
 var user_IP = null; var user_country = null; var user_state = null;
 var browser_name = null; var browser_version; var browser_major_version;
