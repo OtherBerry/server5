4a3b963ea3be71938a7083ff8549f6b29cc11fb7
gilgy1 <gilgy@post.bgu.ac.il>
2024-04-23 13:14:23 +0300

    change video pt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { Pool } = require('pg')
 
-var video_to_reroute = 'https://youtu.be/gm3nFr4hD5U';
+var video_to_reroute = 'https://youtu.be/gZ3xR85DPHE';
 
 var user_IP = null; var user_country = null; var user_state = null;
 var browser_name = null; var browser_version; var browser_major_version;
